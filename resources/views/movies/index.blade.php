34de12651943cf70afbfec988d2b5e01272b5190
FrancescoCesari97 <freedus97@gmail.com>
2024-03-19 22:41:01 +0100

    stampati in pagina i film

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -9,8 +9,31 @@
 
     <section>
         <div class="container py-4">
-            @dump($movies)
+            {{-- @dump($movies) --}}
+
+            <div class="row ">
+                @forelse ($movies as $movie)
+                    <div class="col-6 g-4 ">
+
+                        <div class="card card-body h-100 ">
+                            {{-- <a href="{{ route('comic-detail', [
+                            'index' => $index,
+                        ]) }}"> --}}
+                            </a>
+
+                            <h3 class="fs-6 ">{{ $movie->title }}</h3>
+                        </div>
+                    </div>
+                @empty
+                    <div class="col-12">
+                        No film
+
+                    </div>
+                @endforelse
+            </div>
+
         </div>
+
     </section>
 
 
