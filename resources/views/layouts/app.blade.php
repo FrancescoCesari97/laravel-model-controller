9886efbadb83dad0af0a1e9cc75027a2847ee48f
FrancescoCesari97 <freedus97@gmail.com>
2024-03-19 16:50:58 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ env('APP_NAME', 'Laravel Project') }} - @yield('title', 'My page')</title>
+
+    @vite(['resources/js/app.js'])
+
+    @yield('css')
+</head>
+
+<body>
+    <div class="wrapper">
+
+        @include('partials.header')
+
+
+        <main>
+            @yield('main-content')
+        </main>
+
+
+
+        @include('partials.footer')
+
+
+        @yield('js')
+    </div>
+</body>
+
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-secondary">
+    <div class="container py-4">
+        &copy; {{ env("APP_AUTHOR"), 'Francesco Cesari' }} {{ now()->year}}
+    </div>
+</footer>
