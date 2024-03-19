b7b906ccc86f0e13a49bb25c91aed93b6b41e1d5
FrancescoCesari97 <freedus97@gmail.com>
2024-03-19 17:48:45 +0100

    aggiunto il model class Movie

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+
 class PageController extends Controller
 {
     //* mostra la homepage del sito
     public function index()
     {
+        $movies = Movie::all();
+
+        dd($movies);
+
         return view('home');
     }
 }
